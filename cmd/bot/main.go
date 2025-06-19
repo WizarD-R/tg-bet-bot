47f78a63219609f0941e8d5a960fdb38eb414067
Maxim <mr.loraks@bk.ru>
2025-06-20 00:15:26 +0600

    add: logic main

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"RushBananaBet/internal/user"
+	"RushBananaBet/internal/app"
+	user "RushBananaBet/internal/model"
 	"RushBananaBet/pkg/logger"
 	"os"
+	"os/signal"
+	"syscall"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"gopkg.in/yaml.v3"
 )
 
-var initDataMap map[string]interface{}
+var initData struct {
+	Admins     []string `yaml:"admins"`
+	BotToken   string   `yaml:"botToken"`
+	LogLevel   int      `yaml:"log-level"`
+	Production bool     `yaml:"production"`
+}
 
 func init() {
 	data, err := os.ReadFile("../../configs/config.yml")
@@ -17,35 +24,22 @@ func init() {
 		logger.Fatal("Cant read config", "", "", "main-init()", err)
 	}
 
-	err = yaml.Unmarshal(data, &initDataMap)
+	err = yaml.Unmarshal(data, &initData)
 	if err != nil {
 		logger.Fatal("Cant unmarshal data from config", "", "", "main-init()", err)
 	}
 
-	admins, ok := initDataMap["admins"]
-	if !ok {
-		logger.Fatal("Field 'admins' not found", "", "", "main-init()", err)
-	}
-	user.Admins = admins.([]string)
+	// Set admins
+	user.Admins = initData.Admins
+
+	// Init logger
+	logger.InitLogger(initData.LogLevel, initData.Production)
 }
 
 func main() {
-	botToken, ok := initDataMap["botToken"]
-	if !ok {
-		logger.Fatal("Cant read 'bot token' from configMap", "", "", "main-main()", nil)
-	}
-	bot, err := tgbotapi.NewBotAPI(botToken.(string))
-	if err != nil {
-		logger.Fatal("Error creating newBot", "", "", "main-main()", err)
-	}
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message != nil {
-		}
-	}
+	bot := app.NewBot(initData.BotToken)
+	bot.Start(stop)
 }
